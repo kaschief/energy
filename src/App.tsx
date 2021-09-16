2fdde9bd08acc8149ea0590e6b691054d103417e
Kaschief Johnson <kaschiefj@gmail.com>
2021-09-16 17:55:49 +0200

    main values in place and local state functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Gases, Countries } from "./types.d";
 
 function App() {
+  const [country, setCountry] = useState("DE");
+  const [gas, setGas] = useState("carbonmonoxide");
+
+  const countriesArr = Object.values(Countries);
+  const gasesArr = Object.values(Gases);
+
+  useEffect(() => {
+    console.log(gas, country);
+  }, [gas, country]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          margin: "20px",
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ margin: "0px 5px" }}>{country}</h1>
+        <h1 style={{ margin: "0px 5px" }}>{gas}</h1>
+      </div>
+
+      <div style={{ margin: "20px" }}>
+        {countriesArr.map((country, index) => {
+          return (
+            <button
+              key={index}
+              style={{ margin: "0px 5px" }}
+              onClick={() => {
+                setCountry(country);
+              }}
+            >
+              {country}
+            </button>
+          );
+        })}
+      </div>
+
+      <div style={{ margin: "20px" }}>
+        {gasesArr.map((gas, index) => {
+          return (
+            <button
+              key={index}
+              style={{ margin: "0px 5px" }}
+              onClick={() => {
+                setGas(gas);
+              }}
+            >
+              {gas}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export enum Gases {
+  METHANE = "methane",
+  CARBON_MONOXIDE = "carbonmonoxide",
+  OZONE = "ozone",
+  NITROGEN_DIOXIDE = "nitrogendioxide",
+}
+
+export enum Countries {
+  GERMANY = "DE",
+  FRANCE = "FR",
+  UK = "UK",
+}
